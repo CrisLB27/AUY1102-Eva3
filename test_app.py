a2f84a5481aff925ea077ea58bd6e765f128f583
CrisLB27 <cristoballealbelmar@gmail.com>
2025-11-25 00:01:38 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,6 +1,5 @@
 import pytest
 from app import app
-import json
 
 @pytest.fixture
 def client():
@@ -8,27 +7,13 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_home_json(client):
+def test_home_page_loads(client):
     rv = client.get('/')
     assert rv.status_code == 200
-    json_data = rv.get_json()
-    assert json_data['service'] == "Inventory-API"
-    assert json_data['status'] == "running"
+    assert b"Inventario de Servidores" in rv.data
+    assert b"Servidor Dell PowerEdge" in rv.data
 
-def test_get_all_items(client):
-    rv = client.get('/api/items')
-    assert rv.status_code == 200
-    json_data = rv.get_json()
-    assert json_data['count'] == 3
-
-def test_get_item_found(client):
-    rv = client.get('/api/items/1')
-    assert rv.status_code == 200
-    json_data = rv.get_json()
-    assert json_data['name'] == "Servidor Dell PowerEdge"
-
-def test_get_item_not_found(client):
-    rv = client.get('/api/items/999')
-    assert rv.status_code == 404
-    json_data = rv.get_json()
-    assert "error" in json_data
+def test_item_count(client):
+    rv = client.get('/')
+    assert b"Mostrando 3 \xc3\xadtems" in rv.data
+    assert b"Rack B" in rv.data
